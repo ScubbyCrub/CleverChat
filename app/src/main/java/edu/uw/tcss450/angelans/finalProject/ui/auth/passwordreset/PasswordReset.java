d3118a0bf213ad4ad4a0064c525801dca46cc7a5
Vlad Tregubov <treguv@uw.edu>
2021-11-03 00:23:29 -0700

    added confirmation of email send page

--- app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/auth/passwordreset/PasswordReset.java
+++ app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/auth/passwordreset/PasswordReset.java
@@ -1,10 +1,16 @@
 package edu.uw.tcss450.angelans.finalProject.ui.auth.passwordreset;
 
+import static edu.uw.tcss450.angelans.finalProject.utils.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.tcss450.angelans.finalProject.utils.PasswordValidator.checkPWLength;
+import static edu.uw.tcss450.angelans.finalProject.utils.PasswordValidator.checkPwdSpecialChar;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,16 +18,29 @@ import android.view.ViewGroup;
 
 import edu.uw.tcss450.angelans.finalProject.R;
 import edu.uw.tcss450.angelans.finalProject.databinding.FragmentPasswordResetBinding;
+import edu.uw.tcss450.angelans.finalProject.utils.PasswordValidator;
 
 /**
  */
 public class PasswordReset extends Fragment {
+
+    private PasswordValidator emailCheck = checkPWLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
     private FragmentPasswordResetBinding binding;
     private PasswordResetViewModel passwordResetViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        passwordResetViewModel = new ViewModelProvider(getActivity()).get(PasswordResetViewModel.class);
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view,savedInstanceState);
         //add listener that will send the request to the backend
+        binding.buttonBackToSignIn.setOnClickListener(this::attemptRegister);
+
 
 
 
@@ -29,7 +48,27 @@ public class PasswordReset extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_reset, container, false);
+        //make binding so we can access fields
+        binding = FragmentPasswordResetBinding.inflate(inflater);
+
+        return binding.getRoot();//inflater.inflate(R.layout.fragment_password_reset, container, false);
+    }
+
+    private void attemptRegister(final View button) {checkEmail();}
+
+    private void checkEmail() {
+        emailCheck.processResult(
+                emailCheck.apply(binding.editEmail.getText().toString().trim()),
+                this::sendToServer,
+                result -> binding.editEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void sendToServer() {
+        System.out.println("Sending to the server");
+        passwordResetViewModel.connect(binding.editEmail.getText().toString().trim());
+        //TODO: ADD navigation to fragment that confirms the email was sent
+        Navigation.findNavController(getView()).navigate(
+                PasswordResetDirections.actionPasswordResetToResetEmailSentFragment()
+        );
     }
 }
--- app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/auth/passwordreset/PasswordResetViewModel.java
+++ app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/auth/passwordreset/PasswordResetViewModel.java
@@ -1,14 +1,147 @@
 package edu.uw.tcss450.angelans.finalProject.ui.auth.passwordreset;
 
 import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import edu.uw.tcss450.angelans.finalProject.R;
 
 public class PasswordResetViewModel extends AndroidViewModel {
-
+    private MutableLiveData<JSONObject> response;
     public PasswordResetViewModel(@NonNull Application application){
         super(application);
+        response = new MutableLiveData<>();
+        response.setValue(new JSONObject());
+    }
+    //add obvserver to our data
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        response.observe(owner, observer);
+    }
+    //this is what will actually send the request to our server
+    public void connect(final String email){
+        String url = "https://cleverchat.herokuapp.com/api/passwordreset";
+        JSONObject body = new JSONObject();
+        //add stuff to body
+        try{
+            body.put("userEmail",email);
+        } catch( JSONException e){
+            e.printStackTrace();
+        }
+
+        //make request
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                response:: setValue,
+                this::handleError);
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+    //you done messed up aaron
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                response.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset())
+                    .replace('\"', '\'');
+            try {
+                JSONObject response = new JSONObject();
+                response.put("code", error.networkResponse.statusCode);
+                response.put("data", new JSONObject(data));
+                this.response.setValue(response);
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
     }
 
+    /**
+     * A simple {@link Fragment} subclass.
+     * Use the {@link ResetEmailSentFragment#newInstance} factory method to
+     * create an instance of this fragment.
+     */
+    public static class ResetEmailSentFragment extends Fragment {
+
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_PARAM1 = "param1";
+        private static final String ARG_PARAM2 = "param2";
+
+        // TODO: Rename and change types of parameters
+        private String mParam1;
+        private String mParam2;
+
+        public ResetEmailSentFragment() {
+            // Required empty public constructor
+        }
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ResetEmailSentFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        public static ResetEmailSentFragment newInstance(String param1, String param2) {
+            ResetEmailSentFragment fragment = new ResetEmailSentFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_PARAM1, param1);
+            args.putString(ARG_PARAM2, param2);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+                mParam1 = getArguments().getString(ARG_PARAM1);
+                mParam2 = getArguments().getString(ARG_PARAM2);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_reset_email_sent, container, false);
+        }
+    }
 }
