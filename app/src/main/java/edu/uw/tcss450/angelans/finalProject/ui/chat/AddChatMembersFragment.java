6ceb46da09f3635b41b325ec53fb4c8d07d79e42
Vlad Tregubov <treguv@uw.edu>
2021-12-08 02:48:52 -0800

    added page to display contacts and allow you to select which to add to the chat. adding functionality not yet implemented

--- app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/chat/AddChatMembersFragment.java
+++ app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/chat/AddChatMembersFragment.java
@@ -1,66 +1,92 @@
 package edu.uw.tcss450.angelans.finalProject.ui.chat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.function.Consumer;
+
 import edu.uw.tcss450.angelans.finalProject.R;
+import edu.uw.tcss450.angelans.finalProject.databinding.FragmentAddChatMembersBinding;
+import edu.uw.tcss450.angelans.finalProject.databinding.FragmentNewChatBinding;
+import edu.uw.tcss450.angelans.finalProject.model.Contact;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddChatMembersFragment#newInstance} factory method to
- * create an instance of this fragment.
+ *
  */
 public class AddChatMembersFragment extends Fragment {
+    private FragmentAddChatMembersBinding mBinding;
+    private AddNewChatMembersViewModel mNewChatViewModel;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AddChatMembersFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddChatMembersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddChatMembersFragment newInstance(String param1, String param2) {
-        AddChatMembersFragment fragment = new AddChatMembersFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onCreate(@Nullable Bundle theSavedInstanceState) {
+        super.onCreate(theSavedInstanceState);
+        //get shared prefs
+        SharedPreferences prefs =
+                getActivity().getSharedPreferences(
+                        getString(R.string.keys_shared_prefs),
+                        Context.MODE_PRIVATE);
+        //make needed requests
+        mNewChatViewModel = new ViewModelProvider(getActivity())
+                .get(AddNewChatMembersViewModel.class);
+        //get the contacts of the user
+        mNewChatViewModel.connectGetContacts(prefs.getString("email",""),
+                prefs.getString(getString(R.string.keys_prefs_jwt),""));
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View theView, @Nullable Bundle theSavedInstanceState){
+        super.onViewCreated(theView,theSavedInstanceState);
+        SharedPreferences prefs =
+                getActivity().getSharedPreferences(
+                        getString(R.string.keys_shared_prefs),
+                        Context.MODE_PRIVATE);
+        //old observer for navigating to new chat
+//        mNewChatViewModel.addNewChatObserver(getViewLifecycleOwner(), created -> {
+//            //set up navigation
+//            NewChatFragmentDirections.ActionNewChatFragmentToSingleChatFragment dir =
+//                    NewChatFragmentDirections.actionNewChatFragmentToSingleChatFragment();
+//            dir.setId(mNewChatViewModel.getChatId());
+//            Navigation.findNavController(getView()).navigate(dir);
+//        });
+        //old observer for making new chat
+//        mBinding.buttonCreateNewChat.setOnClickListener(button -> {
+//            //make request
+//            mNewChatViewModel.connectPost(
+//                    mBinding.editTextChatName.getText().toString().trim(),
+//                    prefs.getString(getString(R.string.keys_prefs_jwt),"")
+//            );
+//
+//        });
+        Consumer<Contact> add = (contact) -> mNewChatViewModel.addContact(contact);
+        Consumer<Contact> remove = (contact) -> mNewChatViewModel.removeContact(contact);
+        ContactGeneratorChat stuff = new ContactGeneratorChat();
+        mNewChatViewModel.addSelectedChatObserver(getViewLifecycleOwner(), contactList -> {
+            System.out.println("My Contact: " + contactList.toString());
+            mBinding.listAddContacts.setAdapter(new ContactCardListRecyclerViewAdapter(contactList, add, remove));
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_chat_members, container, false);
+        mBinding = FragmentAddChatMembersBinding.inflate(inflater);
+        return mBinding.getRoot();
     }
 }
---
+++ app/src/main/java/edu/uw/tcss450/angelans/finalProject/ui/chat/AddNewChatMembersViewModel.java
@@ -0,0 +1,208 @@
+package edu.uw.tcss450.angelans.finalProject.ui.chat;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.IntFunction;
+
+import edu.uw.tcss450.angelans.finalProject.R;
+import edu.uw.tcss450.angelans.finalProject.io.RequestQueueSingleton;
+import edu.uw.tcss450.angelans.finalProject.model.Contact;
+
+public class AddNewChatMembersViewModel extends AndroidViewModel {
+    private MutableLiveData<Boolean> mStatus;
+    private MutableLiveData<List<Contact>> mSelectedContacts;
+    private MutableLiveData<List<Contact>> mContacts;
+    private MutableLiveData<Integer> mChatId;
+
+    /**
+     * Constructor for the view model
+     * @param application the application
+     */
+    public AddNewChatMembersViewModel(@NonNull Application application){
+        super(application);
+        mStatus = new MutableLiveData<>();
+        mSelectedContacts = new MutableLiveData<>();
+        mSelectedContacts.setValue(new ArrayList<>());
+        mContacts = new MutableLiveData<>();
+        mContacts.setValue(new ArrayList<>());
+        mChatId = new MutableLiveData<>();
+        mChatId.setValue(0);
+    }
+
+    /**
+     * Add observer to the status data
+     * @param owner owner of the obserrver
+     * @param observer observer datya type
+     */
+    public void addNewChatObserver(@NonNull LifecycleOwner owner,
+                                   @NonNull Observer<? super Boolean> observer){
+        mStatus.observe(owner, observer);
+    }
+
+    /**
+     * Add observer to chat selection data
+     * @param owner observer owner
+     * @param observer observer
+     */
+    public void addSelectedChatObserver(@NonNull LifecycleOwner owner,
+                                        @NonNull Observer<? super List<Contact>> observer){
+        mContacts.observe(owner,observer);
+    }
+
+    /**
+     * Handle any VolleyErrors that come back from web end communication.
+     * @param error The Volley error received from web end communication.
+     */
+    private void handleError(final
+                             VolleyError error) {
+        //you should add much better error handling in a production release.
+        //i.e. YOUR PROJECT
+        //TODO: Make better error handling
+        Log.e("Error.toString", error.getMessage());
+        throw new IllegalStateException(error.getMessage());
+    }
+
+    /**
+     * Handles the result of a successful request
+     * @param result json data objeect with the result
+     */
+    public void handleResult(final JSONObject result) {
+        try{
+            mChatId.setValue(result.getInt("chatid"));
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        mStatus.setValue(true);;
+    }
+
+    /**
+     * Gets a list of users contacts from the database
+     * @param email The email of the user
+     * @param jwt the jwt of the user
+     */
+    public void connectGetContacts(final String email, String jwt) {
+        String baseUrl = getApplication().getResources().getString(R.string.base_url);
+        String url = baseUrl + "contact/list";
+
+        JSONObject body = new JSONObject();
+        try {
+            body.put("email", email);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                this::handleSuccess,
+                this::handleError) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext()).addToRequestQueue(request);
+    }
+
+    /**
+     * Handle a successful input from the web end communication
+     * @param response The JSON web response to a web request
+     */
+    private void handleSuccess(final JSONObject response){
+        IntFunction<String> getString =
+                getApplication().getResources()::getString;
+        try {
+            JSONObject root = response;
+            System.out.println(root.toString());
+            if (root.has("rows")) { //TODO move these hardcoded strings to separate file
+//                //This user is a part of at least one chat
+                JSONArray contactData =
+                        root.getJSONArray("rows");
+                for(int i = 0; i < contactData.length(); i++) {
+                    JSONObject jsonContact = contactData.getJSONObject(i);
+                    Contact aContact = new Contact(jsonContact.getInt("memberid"),
+                            jsonContact.getString("username"),
+                            jsonContact.getString("name"),
+                            jsonContact.getString("username"),
+                            jsonContact.getString("name").split(" ")[0],
+                            jsonContact.getString("name").split(" ")[1]);
+                    boolean contains = false;
+                    for(Contact c : mContacts.getValue()){
+                        if(c.getId() == aContact.getId()){
+                            contains = true;
+                            break;
+                        }
+                    }
+                    if (!contains) {
+                        mContacts.getValue().add(0,aContact);
+                    }
+                }
+            } else {
+                Log.e("ERROR!", "No response");
+            }
+            System.out.println("stuff");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("ERROR!", e.getMessage());
+        }
+
+        mContacts.setValue(mContacts.getValue());
+
+    }
+
+    /**
+     * adds contact to list of selected contacts
+     * @param contact the user's new contact to add
+     */
+    public void addContact(Contact contact){
+        mSelectedContacts.getValue().add(contact);
+        Log.d("selected contacts", "addContact: " + mSelectedContacts.getValue().toString());
+    }
+    /**
+     * remove contact to list of selected contacts
+     * @param contact the user's contact to remove
+     */
+    public void removeContact(Contact contact){
+        mSelectedContacts.getValue().remove(contact);
+        Log.d("selected contacts", "addContact: " + mSelectedContacts.getValue().toString());
+    }
+
+    /**
+     * get the id of the chat
+     * @return id of the chat
+     */
+    public int getChatId(){
+        return mChatId.getValue();
+    }
+}
